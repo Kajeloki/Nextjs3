ccab1e2b5164a1371bf15001c1bcd77a0f2934f7
Kajeloki <piotrkw200@wp.pl>
2023-10-09 12:25:51 +0200

    początki

---
+++ pages/new-meetup/index.js
@@ -0,0 +1,13 @@
+import { Fragment } from "react";
+import MeetupList from "../../components/meetups/MeetupList";
+
+
+
+const NewMeetup = () =>{
+    return <Fragment>
+            <h1>Nowe spotkanie</h1>
+    
+    </Fragment>
+    }
+    
+    export default NewMeetup;
