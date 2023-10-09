f5ec47a3cecf3a89cb3dcca6997e9686ff9d37be
Kajeloki <piotrkw200@wp.pl>
2023-10-09 18:01:45 +0200

    dodanie kilku stron

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,36 @@
 import MeetupList from "../components/meetups/MeetupList";
+import Layout from "../components/layout/Layout";
+import { useEffect, useState } from "react";
 
 const DUMMY_MEETUPS = [
-    {
-        id: 'm1',
-        title: 'A first meetup',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/Munchen_collage.jpg/1024px-Munchen_collage.jpg',
-        address: 'Some address 1, 23456 Some City',
-        description: 'To jest pierwsze spotkanie!' 
-    },
-    {
-        id: 'm2',
-        title: 'A second meetup',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/Munchen_collage.jpg/1024px-Munchen_collage.jpg',
-        address: 'Some address 1, 23456 Some City',
-        description: 'To jest drugie spotkanie!' 
-    }
-]
+  {
+    id: "m1",
+    title: "A first meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/Munchen_collage.jpg/1024px-Munchen_collage.jpg",
+    address: "Some address 1, 23456 Some City",
+    description: "To jest pierwsze spotkanie!",
+  },
+  {
+    id: "m2",
+    title: "A second meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/Munchen_collage.jpg/1024px-Munchen_collage.jpg",
+    address: "Some address 1, 23456 Some City",
+    description: "To jest drugie spotkanie!",
+  },
+];
 
-const Home = () =>{
-return <>
-<h1>Strona Domowa</h1>
-<MeetupList meetups={DUMMY_MEETUPS}/>
-</>
-}
+const Home = () => {
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
+  useEffect(() => {
+    setLoadedMeetups(DUMMY_MEETUPS);
+  }, []);
+  return (
+    <>
+      <MeetupList meetups={loadedMeetups} />
+    </>
+  );
+};
 
 export default Home;
